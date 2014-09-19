5f5ea285532e8b1a77a095c6eecdfe865299565c
Jade <jadedickinson@gmail.com>
2014-09-19 10:38:57 +0100

    saved main file

--- src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
+++ src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
@@ -15,5 +15,10 @@ public class ListSortIntegersToRangesApp {
      */
     public static void main(String[] args) {
         System.out.print("This thing is on");
+        /** Create the range of numbers. Possibly later take an input for,
+         * or generate a random list of numbers. These are in an array.
+         **/
+        
+        
     }
 }
