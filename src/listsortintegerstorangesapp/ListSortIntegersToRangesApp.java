dc720ad19eec853ca4c383be0df9429889debc86
Jade <jadedickinson@gmail.com>
2014-09-19 11:58:34 +0100

    Creating an array of 7 elements, all integers but not in an order.

--- src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
+++ src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package listsortintegerstorangesapp;
-
+import java.util.*;
 /**
  *
  * @author jade
@@ -14,10 +14,16 @@ public class ListSortIntegersToRangesApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("This thing is on");
+        //System.out.print("This thing is on");
         /** Create the range of numbers. Possibly later take an input for,
          * or generate a random list of numbers. These are in an array.
          **/
+        int[] numbers = {5, 3, 4, 3, 12, 14, 20};
+        //Print the entire array, as is.
+        for (int i = 0; i < 7; i++) {
+            //Assumption that number of integers in array is equal to 7.
+           System.out.println(numbers[i]); 
+        }
         
         
     }
