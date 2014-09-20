fa82bf549a79e64a19b9bd8d7d434c9f9bc75a66
Jade <jadedickinson@gmail.com>
2014-09-20 10:02:13 +0100

    Removed printing with :’s and commas to start the converting part of the problem from scratch

--- src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
+++ src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
@@ -1,16 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * idea: assign the sorted numbers to objects. i think that's the term 
- * i mean. then for the next step you'll be able to reassign those to
- * an array
- * 1) Implement the following method:
+/* 1) Implement the following method:
 
 // Convert a list of not-necessarily sequential
 // numbers into a comma-separated list of
 // ranges, of the format "from:to".
 
-static string ranges(int[] numbers) {}
+    static string ranges(int[] numbers) {}
 
 // Example: ranges([1,2]) = "1:2"
 // Example: ranges([1,2,3,5]) = "1:3,5"
@@ -55,33 +49,6 @@ public class ListSortIntegersToRangesApp {
                 System.out.print(integer_in_array[print]+ " ");
             }
             System.out.println();
-            
-            
-                
-        // Commented out to take input of numbers/int[] numbers = {5, 3, 4, 13, 12, 14, 20};
-        //Print the entire array, as is.
-        // Commented/ for (int i = 0; i < 7; i++) {
-        //    out to try input    //Assumption that number of integers in array is equal to 7.
-        //       for these numbers   System.out.println(numbers[i]); 
-            //below i print any numbers where the difference between the number and the next number in the array
-            //is exactly 1. Yes, this works.
-// commented     for (int i = 0; i<5; i++ ) {
-                //if(i=)
-                
-//              if (num_in_array[i]==(num_in_array[i+1]-1)) {
-//                  if(num_in_array[i+1] != (num_in_array[i+2]-1)){
-//                      System.out.println(num_in_array[i]+":"+num_in_array[i+1] );
-//                      i++;
-//                  }
-                    
-
-                    
-//              }
-//              else {
-//                  System.out.println(num_in_array[i]+", ");
-//              }
-                
-//          }
 
     }
 
