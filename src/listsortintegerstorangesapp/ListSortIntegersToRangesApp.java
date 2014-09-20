3ea4f645ff28c24aa06f8915292b71b591341116
Jade <jadedickinson@gmail.com>
2014-09-20 15:54:49 +0100

    Up to fixing the 1:2:3, 5 problem. Have to make the printout into something that can be used.

--- src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
+++ src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
@@ -22,11 +22,10 @@ public class ListSortIntegersToRangesApp {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //System.out.print("This thing is on");
-        /** Create the range of numbers. Possibly later take an input for,
-         * or generate a random list of numbers. These are in an array.
-         **/
-        //Adding this bit inside an interface to make the method static
+
+        /** Create the range of numbers. Take an input for a list of numbers.
+         * These are in an array.**/
+        
         Scanner input = new Scanner(System.in);
         
             System.out.print("Type 7 numbers to sort: ");
@@ -88,7 +87,5 @@ public class ListSortIntegersToRangesApp {
 
     }
 
-        //check
-
         
-    }
+}
