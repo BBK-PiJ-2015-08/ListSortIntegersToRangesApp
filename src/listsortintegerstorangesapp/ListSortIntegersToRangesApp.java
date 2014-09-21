7abc84a5a49b16779fdb19c22c73f8f5121a9d4a
Jade <jadedickinson@gmail.com>
2014-09-21 12:45:25 +0100

    Have re-written the if else section, so that each string assignment can only be done by one position in the index. Code still includes commented-out old version of if else area.

--- src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
+++ src/listsortintegerstorangesapp/ListSortIntegersToRangesApp.java
@@ -33,7 +33,7 @@ public class ListSortIntegersToRangesApp {
             static String stringI;
             static String stringJ;
             static String stringK;
-            
+
     public static void main(String[] args) {
         //System.out.print("This thing is on");
         /** Create the range of numbers. Possibly later take an input for,
@@ -71,54 +71,176 @@ public class ListSortIntegersToRangesApp {
                 //The first if statement below handles what to do if the next number is the same as the current number.
                 //When this is the case it will not print the current number, because to avoid duplicate numbers only the
                 //final identical number in the sorted sequence prints.
+                if     (i==0)    {
+                    if (numbers[i] == numbers[i+1]) {
+                        stringA = Integer.toString(numbers[i])+":";
+                        System.out.print(stringA);
+                    }
+                    else if (numbers[i] != (numbers[i+1]-1)) {
+                        stringA = Integer.toString(numbers[i])+",";
+                        System.out.print(stringA);
+                    }
+                    else if (numbers[i] == (numbers[i+1]-1)) {
+                        stringA = Integer.toString(numbers[i])+":";
+                        System.out.print(stringA);
+                    }
+                }
+
+                else if (i==1)   {
+                    if (numbers[i] == numbers[i-1] || numbers[i] == numbers[i+1]) {
+                        stringB = Integer.toString(numbers[i])
+                    }
+                    else if(numbers[i] != (numbers[i+1]-1)) {
+                        stringB = Integer.toString(numbers[i])+",";
+                        System.out.print(stringB);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] != (numbers[i-1]+1) ) {
+                        stringB = Integer.toString(numbers[i])+":";
+                        System.out.print(stringB);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] == (numbers[i-1]+1) ) {
+                        stringB = Integer.toString(numbers[i]);
+                    }
+
+                }
+
+                else if (i==2)   {
+                    if (numbers[i] == numbers[i-1] || numbers[i] == numbers[i+1]) {
+                        stringC = Integer.toString(numbers[i]);
+                    }
+                    else if(numbers[i] != (numbers[i+1]-1)) {
+                        stringC = Integer.toString(numbers[i])+",";
+                        System.out.print(stringC);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] != (numbers[i-1]+1) ) {
+                        stringC = Integer.toString(numbers[i])+":";
+                        System.out.print(stringC);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] == (numbers[i-1]+1) ) {
+                        stringC = Integer.toString(numbers[i]);
+                    }
+                }
+
+                else if (i==3)   {
+                    if (numbers[i] == numbers[i-1] || numbers[i] == numbers[i+1]) {
+                        stringD = Integer.toString(numbers[i]);
+                    }
+                    else if(numbers[i] != (numbers[i+1]-1)) {
+                        stringD = Integer.toString(numbers[i])+",";
+                        System.out.print(stringD);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] != (numbers[i-1]+1) ) {
+                        stringD = Integer.toString(numbers[i])+":";
+                        System.out.print(stringD);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] == (numbers[i-1]+1) ) {
+                        stringD = Integer.toString(numbers[i]);
+                    }
+                }
+
+                else if (i==4)   {
+                    if (numbers[i] == numbers[i-1] || numbers[i] == numbers[i+1]) {
+                        stringE = Integer.toString(numbers[i]);
+                    }
+                    else if(numbers[i] != (numbers[i+1]-1)) {
+                        stringE = Integer.toString(numbers[i])+",";
+                        System.out.print(stringE);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] != (numbers[i-1]+1) ) {
+                        stringE = Integer.toString(numbers[i])+":";
+                        System.out.print(stringE);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] == (numbers[i-1]+1) ) {
+                        stringE = Integer.toString(numbers[i]);
+                    }
+                }
+
+                else if (i==5)   {
+                    if (numbers[i] == numbers[i-1] || numbers[i] == numbers[i+1]) {
+                        stringF = Integer.toString(numbers[i]);
+                    }
+                    else if(numbers[i] != (numbers[i+1]-1)) {
+                        stringF = Integer.toString(numbers[i])+",";
+                        System.out.print(stringF);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] != (numbers[i-1]+1) ) {
+                        stringF = Integer.toString(numbers[i])+":";
+                        System.out.print(stringF);
+                    }
+                    else if(numbers[i] == (numbers[i+1]-1) && numbers[i] == (numbers[i-1]+1) ) {
+                        stringF = Integer.toString(numbers[i]);
+                    }
+                }
+
+                else if (i==6)   {
+                    stringG = Integer.toString(numbers[i]);
+                    System.out.print(stringG);
+                    break;
+                }
+/**
+///////////////Below is the original code
+                //have just replicated this for the first index.
                 if(i<6 && i!=0 && numbers[i] == (numbers[i+1])) {
                     stringJ = Integer.toString(numbers[i]);
                 }
+                //Have replicated this 
                 else if (i==6) {
                     stringK = Integer.toString(numbers[i]);
                     System.out.print(stringK);
                     break;
                 }
+                //Have replicated this
                 else if (i==0 && numbers[i] == numbers[i+1]) {
                     stringA = Integer.toString(numbers[i])+":";
                     System.out.print(stringA);
                 }
+                //Have replicated this
                 else if (i==0 && numbers[i] != (numbers[i+1]-1)) {
                     stringB = Integer.toString(numbers[i])+",";
                     System.out.print(stringB);
                     
                 }
+                //Have replaced this by using an OR (||) condition if number is same as next or same as previous.
+                //Have replicated this
                 else if (i==0 && numbers[i] == (numbers[i+1]-1)) {
                     stringC = Integer.toString(numbers[i])+":";
                     System.out.print(stringC);
                 }
+                //Have replaced this by using an OR (||) condition if number is same as next or same as previous.
                 //If the current number is the same as the previous number and the next number, do not print anything.
                 else if (numbers[i] == numbers[i-1] && numbers[i] == numbers[i+1]){
                     stringD = Integer.toString(numbers[i]);
                 }
+                //Have replaced this by using an OR (||) condition if number is same as next or same as previous.
                 //If the current number is preceded by a consecutive number and the current number is the same as the next number, don't print.
                 else if (numbers[i] == (numbers[i-1]+1) && numbers[i] == numbers[i+1]){
                     stringE = Integer.toString(numbers[i]);
                 }
+                //Have replaced this by using an OR (||) condition if number is same as next or same as previous.
                 //If the current number is the same as the previous number and followed by a consecutive number, don't print it.
                 else if (numbers[i] == (numbers[i-1]) && numbers[i] == (numbers[i+1])-1 ){
                     stringF = Integer.toString(numbers[i]);
                 }
+                //Have replicated this for index positions 1-5
                 else if(numbers[i] != (numbers[i+1]-1)) {
                     stringG = Integer.toString(numbers[i])+",";
                     System.out.print(stringG);
                 }
+                //Have replicated this for index positions 1-5
                 else if(numbers[i] == (numbers[i+1]-1) && numbers[i] != (numbers[i-1]+1) ) {
                     stringH = Integer.toString(numbers[i])+":";
                     System.out.print(stringH);
                 }
+                //Have replicated this for index positions 1-5
                 else if(numbers[i] == (numbers[i+1]-1) && numbers[i] == (numbers[i-1]+1) ) {
                     stringI = Integer.toString(numbers[i]);
                 }
+                //Probably not necessary.
                 else {
                     System.out.println();
-                }
-
+                } 
+                **/
+//Bracket below is the closure of the for loop.
             }
             System.out.println();
             //System.out.println("A string: " + stringB + stringG + stringK);
